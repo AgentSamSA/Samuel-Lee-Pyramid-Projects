da9291957e5c7915447c2680efcda22d930d2309
Samuel Lee <samuelxlee@gmail.com>
2022-07-18 14:13:43 -0400

    Added exception handling to Dragon Cave

--- Dragon Cave/DragonCave.java
+++ Dragon Cave/DragonCave.java
@@ -1,7 +1,17 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
 
+class InvalidInputException extends Exception {
+}
+
 public class DragonCave {
+
+    static void checkInput(int input) throws InvalidInputException {
+        if (input != 1 && input != 2) {
+            throw new InvalidInputException();
+        }
+    }
     public static void main(String[] args) {
         Random rand = new Random();
         int badDragon = rand.nextInt(2) + 1;
@@ -14,16 +24,25 @@ public class DragonCave {
         System.out.println("is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you go into? (Enter 1 or 2)");
 
-        int userInput = scanner.nextInt();
+        try {
+            int userInput = scanner.nextInt();
+            checkInput(userInput);
 
-        System.out.println("You approach the cave...");
-        System.out.println("It is dark and spooky...");
-        System.out.println("A large dragon dumps out in front of you! He opens his jaws and...");
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon dumps out in front of you! He opens his jaws and...");
 
-        if (userInput == badDragon) {
-            System.out.println("Gobbles you down in one bite!");
-        } else {
-            System.out.println("Laughs and offers you a piece of treasure!");
+            if (userInput == badDragon) {
+                System.out.println("Gobbles you down in one bite!");
+            } else {
+                System.out.println("Laughs and offers you a piece of treasure!");
+            }
+        } catch (InputMismatchException ime) {
+            System.out.println("Exception: Must input an integer");
+            System.exit(1);
+        } catch (InvalidInputException iie) {
+            System.out.println("Exception: Must input 1 or 2");
+            System.exit(1);
         }
 
         scanner.close();
